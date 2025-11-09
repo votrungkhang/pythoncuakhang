de46bab410ff048c4e4b1eb5f05856e929e3b085
votrungkhang <trungkhang1704@gmail.com>
2025-11-09 16:41:55 +0700

    toi moi them vao ddoanj code mang y nghia la neu chia het cho 2 thi in ra vo trung khang nguoc lai in ra vo thi xuan quynh

--- khang.py
+++ khang.py
@@ -2,3 +2,8 @@ n=int(input())
 for i in range(n):
     print("anh khang dẹp trai nhất trên đời")
     print("tôi yêu anh")
+for i in range(n):
+    if (i%2==0):
+        print("vo trung khang")
+    else:
+        print("vox thi xuan quynh")
